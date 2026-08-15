fd413992cc31792eeb94680e8242b93e8f3ef6c8
Iurii Medynskii <mjv.1981@gmail.com>
2026-08-16 02:54:04 +0400

    CHANGE-001: increment counter by 2

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -11,12 +11,19 @@ test('REG-001 - home page loads correctly', async ({ page }) => {
       page.getByRole('heading', { name: 'Get started' })
     ).toBeVisible();
 
-    await expect(
-      page.getByRole('button')
-    ).toContainText('Count is 0');
+    const counter = page.getByRole('button');
+
+    await expect(counter).toContainText('Count is 0');
+
+    await counter.click();
+    await expect(counter).toContainText('Count is 2');
+
+    await counter.click();
+    await expect(counter).toContainText('Count is 4');
   });
 
   await test.step('Verify visual regression', async () => {
+    await page.reload();
     await expect(page).toHaveScreenshot('home-page.png', {
       fullPage: true,
     });
